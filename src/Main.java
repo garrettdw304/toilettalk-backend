ef5cf26d34b7c5fabf3b81690d41d75c8bab126b
Garrettdw304 <garrettdw14497@outlook.com>
2025-03-11 08:33:00 -0400

    Dont crash on null .env args

--- src/Main.java
+++ src/Main.java
@@ -13,33 +13,40 @@ public class Main {
             throw new RuntimeException(e);
         }
 
-        HttpServer server;
-        try {
-            server = HttpServer.create(new InetSocketAddress(9500), -1);
-        } catch (IOException e) {
-            throw new Error(e);
+        HttpServer server = null;
+
+        if (Env.INIT_SUCCESSFUL) {
+            try {
+                server = HttpServer.create(new InetSocketAddress(9500), -1);
+            } catch (IOException e) {
+                throw new Error(e);
+            }
+
+            server.createContext("/api/signUp",
+                    e -> ReqHandlers.handleUncaughtExceptions(ReqHandlers::signUp, e));
+            server.createContext("/api/signIn",
+                    e -> ReqHandlers.handleUncaughtExceptions(ReqHandlers::signIn, e));
+            server.createContext("/api/refreshAccess",
+                    e -> ReqHandlers.handleUncaughtExceptions(ReqHandlers::refreshAccess, e));
+            server.createContext("/api/getReviews",
+                    e -> ReqHandlers.handleUncaughtExceptions(ReqHandlers::getReviews, e));
+            server.createContext("/api/createReview",
+                    e -> ReqHandlers.handleUncaughtExceptions(ReqHandlers::createReview, e));
+            server.createContext("/api/getBathrooms",
+                    e -> ReqHandlers.handleUncaughtExceptions(ReqHandlers::getBathrooms, e));
+            server.createContext("/api/getBuildings",
+                    e -> ReqHandlers.handleUncaughtExceptions(ReqHandlers::getBathrooms, e));
+
+            server.start();
+        } else {
+            System.err.println("ENV initialization failed. THE SERVER IS NOT RUNNING! Fix the errors and relaunch the program.");
         }
-
-        server.createContext("/api/signUp",
-                e -> ReqHandlers.handleUncaughtExceptions(ReqHandlers::signUp, e));
-        server.createContext("/api/signIn",
-                e -> ReqHandlers.handleUncaughtExceptions(ReqHandlers::signIn, e));
-        server.createContext("/api/refreshAccess",
-                e -> ReqHandlers.handleUncaughtExceptions(ReqHandlers::refreshAccess, e));
-        server.createContext("/api/getReviews",
-                e -> ReqHandlers.handleUncaughtExceptions(ReqHandlers::getReviews, e));
-        server.createContext("/api/createReview",
-                e -> ReqHandlers.handleUncaughtExceptions(ReqHandlers::createReview, e));
-        server.createContext("/api/getBathrooms",
-                e -> ReqHandlers.handleUncaughtExceptions(ReqHandlers::getBathrooms, e));
-        server.createContext("/api/getBuildings",
-                e -> ReqHandlers.handleUncaughtExceptions(ReqHandlers::getBathrooms, e));
-
-        server.start();
 
         Commands.Repl();
 
-        System.out.println("Stopping, this may take ~10 seconds.");
-        server.stop(10);
+        if (Env.INIT_SUCCESSFUL) {
+            System.out.println("Stopping, this may take ~10 seconds.");
+            server.stop(10);
+        }
     }
 }
